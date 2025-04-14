1f0bdc733f0cb11e57bdafa35e524b5f0c9fb976
Rafael Bertholdo <rafabertholdo@gmail.com>
2025-04-14 11:56:26 -0300

    dap and keybindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,8 +1,9 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<C-g>", ":NvimTreeToggle<CR>", { desc = "Toggle NvTree" })
@@ -29,8 +30,53 @@ vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab"
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- Debugger
-vim.keymap.set("n", "<leader>db", "<cmd>DapToggleBreakpoint <CR>", { desc = "Toggle breakpoint" }) --  move current buffer to new tab
-vim.keymap.set("n", "<leader>dr", "<cmd>DapContinue <CR>", { desc = "Start or continue the debugger" }) --  move current buffer to new tab
+vim.keymap.set("n", "<C-b>", "<cmd>DapToggleBreakpoint <CR>", { desc = "Toggle breakpoint" }) --  move current buffer to new tab
+--vim.keymap.set("n", "<C-A-y>", "<cmd>DapContinue <CR>", { desc = "Start or continue the debugger" }) --  move current buffer to new tab
 vim.keymap.set("n", "<F6>", "<cmd>DapStepOver<CR>", { desc = "Step Over" })
 vim.keymap.set("n", "<F7>", "<cmd>DapStepInto<CR>", { desc = "Step Into" })
 vim.keymap.set("n", "<F8>", "<cmd>DapStepOut<CR>", { desc = "Step out" })
+
+-- Move
+vim.keymap.set("v", "<C-d>", "<C-d>zz", { noremap = true })
+vim.keymap.set("v", "<C-u>", "<C-u>zz", { noremap = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+local dap_helper = require("dap-helper")
+local dap = require("dap")
+vim.keymap.set("n", "<F5>", function()
+	-- Check if debuggger is already running
+	if #dap.status() == 0 and dap_helper.get_build_cmd() then
+		local ret = os.execute(dap_helper.get_build_cmd() .. " > /dev/null 2>&1")
+		if ret ~= 0 then
+			vim.notify("Build failed", vim.log.levels.ERROR)
+			return
+		end
+	end
+	dap.continue()
+end)
+
+-- vim.keymap.set(
+-- 	"n",
+-- 	"<leader>dt",
+-- 	"<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>",
+-- 	{ desc = "Toggle breakpoint", noremap = true, silent = true }
+-- )
+-- vim.keymap.set(
+-- 	"n",
+-- 	"<leader>dz",
+-- 	"<cmd>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<cr>",
+-- 	{ desc = "Set conditional Breakpoint", noremap = true, silent = true }
+-- )
+-- vim.keymap.set(
+-- 	"n",
+-- 	"<leader>dc",
+-- 	"<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<cr>",
+-- 	{ desc = "clear all breakpoints", noremap = true, silent = true }
+-- )
+-- vim.keymap.set(
+-- 	"n",
+-- 	"<leader>dl",
+-- 	"<cmd>lua require('persistent-breakpoints.api').set_log_point()<cr>",
+-- 	{ desc = "Set log point", noremap = true, silent = true }
+-- )
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,38 +1,54 @@
 return {
-  {
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "nvim-neotest/nvim-nio",
-    },
-    config = function()
-      local dap = require "dap"
-      local dapui = require "dapui"
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "mfussenegger/nvim-dap",
-    },
-    opts = {
-      handlers = {},
-    },
-  },
-  {
-    "mfussenegger/nvim-dap",
-  },
+	{
+		"rcarriga/nvim-dap-ui",
+		event = "VeryLazy",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		opts = {
+			handlers = {},
+		},
+	},
+	{
+		"mfussenegger/nvim-dap",
+	},
+	{
+		"daic0r/dap-helper.nvim",
+		dependencies = { "rcarriga/nvim-dap-ui", "mfussenegger/nvim-dap" },
+		config = function()
+			require("dap-helper").setup()
+		end,
+	},
+	-- {
+	-- 	"Weissle/persistent-breakpoints.nvim",
+	-- 	config = function()
+	-- 		require("persistent-breakpoints").setup({
+	-- 			always_reload = true,
+	-- 			load_breakpoints_event = { "BufReadPost" },
+	-- 		})
+	-- 	end,
+	-- },
 }
